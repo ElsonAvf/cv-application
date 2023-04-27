e9de6c3c6711474a17491f9ccc9dadd0f1fc6c33
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-04-27 17:05:07 -0300

    Create Main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import Description from './../Description/Description.jsx';
+import Education from './../Education/Education.jsx';
+import Experience from './../Experience/Experience.jsx';
+import './Main.css'
+
+export default function Main() {
+  return (
+    <main id='main'>
+      <Description />
+      <Education />
+      <Experience />
+    </main>
+  );
+};
