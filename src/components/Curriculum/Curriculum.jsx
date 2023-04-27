bc335c5b973dc2112ef03e495eec9c3e2bdd0ee6
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-04-27 17:04:53 -0300

    Create Curriculum component

---
+++ src/components/Curriculum/Curriculum.jsx
@@ -0,0 +1,12 @@
+import Main from './../Main/Main.jsx';
+import Aside from './../Aside/Aside.jsx';
+import './Curriculum.css'
+
+export default function Curriculum() {
+  return (
+    <div id='curriculum'>
+      <Aside />
+      <Main />
+    </div>
+  );
+};
